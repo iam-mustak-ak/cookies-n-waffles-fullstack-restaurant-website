67c8bea82b5d3d0f9e6673d39691d266f42f19af
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2024-11-11 11:42:17 +0600

    added variables in frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                primary: "#472515",
+                secondary: "#C27D5C",
+                textColor: "#686A79",
+                secondaryTextColor: "#191A23",
+            },
+            fontFamily: {
+                dmSans: "var(--font-dm-sans)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
